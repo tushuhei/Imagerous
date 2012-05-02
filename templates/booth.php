4bfe196194b0948e07f37839db1294411a1d87d2
Shuhei Iitsuka <tushuhei@gmail.com>
2012-05-02 17:29:36 +0900

    edited booth.php

--- templates/booth.php
+++ templates/booth.php
@@ -1,21 +1,8 @@
 <? if (!empty($result)): ?>
 <div class="alert"><?=$result[0]?></div>
 <? endif ?>
-<? for($i = 0; $i < count($article->contents); $i++): ?>
-<a class="main_image" href="picture.php?article=<?=$article->id?>&image=<?=$article->contents[$i]['id']?>">
-    <img src="<?=$article->contents[$i]['image']?>" style="vertical-align:top">
+<? for($i = 0; $i < count($article->pictures); $i++): ?>
+<a class="main_image" href="picture.php?article=<?=$article->id?>&image=<?=$article->pictures[$i]->id?>">
+    <img src="<?=$article->pictures[$i]->small?>" style="vertical-align:top">
 </a>
-<? if ($i % 11 == 10): ?>
-<script type="text/javascript"><!--
-google_ad_client = "ca-pub-6865664974975544";
-/* imagerous-picture */
-google_ad_slot = "2086038225";
-google_ad_width = 300;
-google_ad_height = 250;
-//-->
-</script>
-<script type="text/javascript"
-src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-</script>
-<? endif ?>
 <? endfor ?>
